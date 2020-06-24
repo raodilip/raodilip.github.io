6d8540f6dd6c919715e42ffbde3a0c0881a64b5e
dilip rao <raodilip54@gmail.com>
2020-06-23 20:30:07 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
  $('.header').height($(window).height());
 
+ $('.nav a').on('click', function(){
+    $(".navbar-toggle").click();
+});
+
  $(".navbar a").click(function(){
  	$("body,html").animate({
  		scrollTop:$("#" + $(this).data('value')).offset().top
