08398a09b473dd898b2b5d277bbc62586c971fa4
dilip rao <raodilip54@gmail.com>
2020-06-23 20:32:14 -0700

    update

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@ $(document).ready(function(){
  $('.header').height($(window).height());
 
  $('.nav a').on('click', function(){
-    $(".navbar-toggle").click();
+	$("body,html").animate({
+		scrollTop:$("#" + $(this).data('value')).offset().top
+	},1000)
 });
 
- $(".navbar a").click(function(){
- 	$("body,html").animate({
- 		scrollTop:$("#" + $(this).data('value')).offset().top
- 	},1000)
+//  $(".navbar a").click(function(){
+//  	$("body,html").animate({
+//  		scrollTop:$("#" + $(this).data('value')).offset().top
+//  	},1000)
   
- })
+//  })
 
 })
