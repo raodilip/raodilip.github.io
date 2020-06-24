b9ddbb1d48e8433118216967846d067e876339b0
dilip rao <raodilip54@gmail.com>
2020-06-23 21:03:18 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 $(document).ready(function(){
  $('.header').height($(window).height());
 
- $('.nav a').on('click', function(){
-	$("body,html").animate({
-		scrollTop:$("#" + $(this).data('value')).offset().top
-	},1000)
-});
+//  $('.navbar a').on('click', function(){
+   
+// });
 
-//  $(".navbar a").click(function(){
-//  	$("body,html").animate({
-//  		scrollTop:$("#" + $(this).data('value')).offset().top
-//  	},1000)
+ $(".navbar a").click(function(){
+	 alert();
+ 	$("body,html").animate({
+ 		scrollTop:$("#" + $(this).data('value')).offset().top
+ 	},1000)
   
-//  })
+ })
 
 })
